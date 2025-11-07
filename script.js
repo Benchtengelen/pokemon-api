469ae02f2c1286b3d82dd5a9993cf670f2a1507d
Feyyaz Yılmaz <fyyz.ylmz.bjk@gmail.com>
2025-11-07 21:06:02 +0300

    convert fetch logic to async arrow function

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-async function fetchData(){
+const fetchData = async () => {
         const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
         const errorElement = document.getElementById("errorMessage");
         const imgElement = document.getElementById("pokemonSprite");
-     try {
+    try {
       if (!pokemonName) {
       throw new Error("Please enter a Pokémon name");
     }
-       
-        
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
 
         if(!response.ok) {
             throw new Error("Pokemon not found");
@@ -22,10 +21,10 @@ async function fetchData(){
           imgElement.style.display = "block";
           
            errorElement.textContent = "";
-     } catch (error) {
+    
+    
+    } catch (error) {
          errorElement.textContent = error.message;
          imgElement.style.display = "none";
-         
-     }
-
-}
+    }
+};
